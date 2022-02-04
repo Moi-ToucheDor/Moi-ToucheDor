f05daa6706b089dfb20af311b24075c25cd5055f
Moi-ToucheDor <98697305+Moi-ToucheDor@users.noreply.github.com>
2022-02-04 21:11:28 +0530

    Add files via upload

---
+++ exer01.java
@@ -0,0 +1,21 @@
+package welcom.java;
+
+class exer01 {
+
+	public exer01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]arr= {1,2,3,4,5};
+		System.out.println(arr.length);
+		System.out.println(arr[0]);
+		System.out.println(arr[4]);
+		//it is for human to read the machine will not read this commaand which begins with "//"
+		System.out.println("do you understand?");
+		
+
+	}
+
+}
