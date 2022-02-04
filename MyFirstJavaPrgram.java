b1ee92e962693795a1ae921d4e5ef4a1487482b3
Moi-ToucheDor <98697305+Moi-ToucheDor@users.noreply.github.com>
2022-02-04 21:09:06 +0530

    Add files via upload

---
+++ MyFirstJavaPrgram.java
@@ -0,0 +1,19 @@
+package exemple;
+
+public class MyFirstJavaPrgram {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Bonjour Je suis heueux d'avoir creer une phrase en Java");
+System.out.println("Hi,I am happy to have created a prgram in Java");
+
+	}
+}
+	
+		
+
+		
+	
+
+
